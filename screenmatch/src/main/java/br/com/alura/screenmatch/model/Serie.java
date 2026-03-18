a1df6a80459036f623a37bdf59c4dacfc2940ea5
GuilhermePelissari <guilhermepelissari.feitosa@autbank.com.br>
2026-03-18 11:27:04 -0300

    trazendo episodios do banco

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -25,7 +25,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
@@ -46,6 +46,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(episodio -> episodio.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -122,6 +123,7 @@ public class Serie {
                 ", avaliacao=" + avaliacao +
                 ", atores='" + atores + '\'' +
                 ", poster='" + poster + '\'' +
-                ", sinopse='" + sinopse + '\'';
+                ", sinopse='" + sinopse + '\''+
+                ", episodios='" + episodios + '\'';
     }
 }
