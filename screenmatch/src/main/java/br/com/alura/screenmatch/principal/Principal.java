1dfb67145fb2e82a843dfa63664f1777396661f9
GuilhermePelissari <guilhermepelissari.feitosa@autbank.com.br>
2026-03-31 10:43:38 -0300

    implementando busca de episodio por trecho

--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -39,6 +39,7 @@ public class Principal {
                     6 - Top 5 séries
                     7 - Buscar series por categoria
                     8 - Filtrar series por temporadas e avaliação
+                    9 - Buscar episódios por trecho
                     
                     0 - Sair                                 
                     """;
@@ -71,6 +72,9 @@ public class Principal {
                     break;
                 case 8:
                     filtrarSeriesPorTemporadaEAvaliacao();
+                    break;
+                case 9:
+                    buscarEpisodioPorTrecho();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -182,5 +186,16 @@ public class Principal {
         System.out.println("*** Séries filtradas ***");
         filtroSeries.forEach(s ->
                 System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarEpisodioPorTrecho(){
+        System.out.println("Digite o nome do episódio para busca: ");
+        var trechoEpisodio = leitura.nextLine();
+
+        List<Episodio> episodiosEncontrados = repositorio.episodiosPorTrecho(trechoEpisodio);
+        episodiosEncontrados.forEach(e ->
+                System.out.printf("Série: %s Temporada %s - Episódio %s - %s\n",
+                        e.getSerie().getTitulo(), e.getTemporada(),
+                        e.getNumeroEpisodio(), e.getTitulo()));
     }
 }
